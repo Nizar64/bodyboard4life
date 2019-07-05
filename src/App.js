10ec1986e72c3fd647d40571af27e849efc3b852
slama nizar <slamanizar@MacBook-Pro-de-slama.local>
2019-07-05 10:28:11 +0200

    adding player video

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './menu'
 import Carousel from './carousel';
+import "../node_modules/video-react/dist/video-react.css"; // import css
+import { Player } from 'video-react';
 
 
 function App() {
@@ -10,6 +12,11 @@ function App() {
    <div> 
       <Menu />
       <Carousel id="car"/>
+      <Player
+      playsInline
+      poster="/assets/poster.png"
+      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    />
 }
       <h2 id="t2">Coucou</h2>
      
