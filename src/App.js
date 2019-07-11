956812fbf580c80721a9292d217e539197b0a5ce
slama nizar <ns8@hotmail.fr>
2019-07-11 10:04:21 +0200

    adding youtube player and color

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './menu'
 import Carousel from './carousel';
 import "../node_modules/video-react/dist/video-react.css"; // import css
-import { Player } from 'video-react';
-
+//import { Player } from 'video-react';
+import YouTube from './Youtube';
 
 function App() {
   return (
-   <div> 
+      
+   <div className= "Body"> 
       <Menu />
       <Carousel id="car"/>
       <br/>
@@ -17,18 +19,23 @@ function App() {
       <h1 id="t2"style= {{height:'100%', fontSize:'60px',color:'red'}}>Videos</h1>
       </div>
       <br/>
-      <Player
-      playsInline
-      poster="/assets/poster.png"
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-    />
-}
-     
+      
+      <YouTube />
+        
+        </div>
 
-  
-   </div>
   );
 }
+
+
+
+//Player
+//playsInline
+//poster="/assets/poster.png"
+//src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+// />
+
+//}
 
 
 export default App;
---
+++ src/Youtube.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+class Youtube extends Component {
+    render (){ return (
+        <div className="Youtube">
+            
+
+        <iframe 
+        width="260" height="170" 
+        title='video_Youtube'
+        src="https://www.youtube.com/embed/nWJmNj9vQ-U" 
+        frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen
+        />
+          <iframe 
+        width="260" height="170" 
+        title='video_Youtube'
+        src="https://www.youtube.com/embed/nWJmNj9vQ-U" 
+        frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen
+        />
+         <iframe 
+        title='video_Youtube'
+        width="260" height="170" 
+        src="https://www.youtube.com/embed/nWJmNj9vQ-U" 
+        frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen
+        />
+         <iframe 
+        title='video_Youtube'
+        width="260" height="170" 
+        src="https://www.youtube.com/embed/nWJmNj9vQ-U" 
+        frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen
+        />
+        
+        </div>
+       
+        
+    );
+
+    }
+} 
+ 
+export default Youtube;
